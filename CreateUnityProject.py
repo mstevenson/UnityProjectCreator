ab3bf6ea6c7437094fd260f4ad14292f22f3e131
Michael Stevenson <michael@mstevenson.net>
2018-12-11 21:21:52 -0800

    Code cleanup, copy empty main scene file

--- CreateUnityProject.py
+++ CreateUnityProject.py
@@ -14,13 +14,13 @@
 # - Import Oculus Integration from the Asset Store, or as a submodule.
 # - Set the company and product name in Unity meta files
 # - Enforce Unix line endings in Unity and IDE
+# - Replace Unity project GUID and main scene GUID
 
 import os
 import os.path
 import shutil
 import configparser
 
-template_dir = 'templates'
 config = configparser.ConfigParser()
 
 def create_dir(path):
@@ -28,36 +28,12 @@ def create_dir(path):
 		os.makedirs(path)
 
 def copy_template(source, dest):
+	template_dir = 'templates'
 	s = os.path.join(template_dir, source)
 	if (os.path.isfile(s)):
 		shutil.copy2(s, dest)
 	else:
 		shutil.copytree(s, dest)
-
-def initialize_git(path):
-	copy_template('gitignore', os.path.join(path, '.gitignore'))
-	copy_template('gitattributes', os.path.join(path, '.gitattributes'))
-
-def create_unity_project(path, company, product, unity_version):
-	path = os.path.expanduser(path)
-	if os.path.exists(path):
-		return False
-	create_dir(os.path.join(path, 'Assets'))
-	project_settings_dir = os.path.join(path, 'ProjectSettings')
-	copy_template('ProjectSettings', project_settings_dir)
-	for filename in os.listdir(project_settings_dir):
-		filepath = os.path.join(project_settings_dir, filename)
-		with open(filepath, 'r') as f:
-			text = f.read()
-			text = text.replace(r'%%DefaultCompany%%', company)
-			text = text.replace(r'%%DefaultProduct%%', product)
-			text = text.replace(r'%%UnityVersion%%', unity_version)
-		with open(filepath, "w") as f:
-			f.write(text)
-	return True
-
-def create_ide_files(path):
-	copy_template('editorconfig', os.path.join(path, '.editorconfig'))
 
 if __name__ == '__main__':
 	config_path = 'config.ini'
@@ -66,7 +42,6 @@ if __name__ == '__main__':
 	else:
 		config['Settings'] = {
 			'company' : 'Default Company',
-			'product' : 'Default Product',
 			'unity_version' : '2017.4.17f1',
 			#'timestep' : '0.0111111',
 			#'enable_vr' : 'true'
@@ -74,12 +49,43 @@ if __name__ == '__main__':
 		with open(config_path, 'w') as configfile:
 			config.write(configfile)
 
-	path = raw_input('Enter a new Unity project folder path: ')
-	success = create_unity_project(path, config['Settings']['company'], config['Settings']['product'], config['Settings']['unity_version'])
-	if not success:
+	product = raw_input('Enter a Unity project name: ')
+
+	output_dir = "output"
+	create_dir(output_dir)
+	path = os.path.join(output_dir, product)
+
+	if os.path.exists(path):
 		print('Project already exists')
 		quit()
-	create_ide_files(path)
-	initialize_git(path)
+
+	create_dir(path)
+
+	company = config['Settings']['company']
+	unity_version = config['Settings']['unity_version']
+
+	# Create Unity project
+	create_dir(os.path.join(path, 'Assets'))
+	project_settings_dir = os.path.join(path, 'ProjectSettings')
+	copy_template('ProjectSettings', project_settings_dir)
+	copy_template('Main.unity', os.path.join(path, 'Assets', 'Main.unity'))
+	for filename in os.listdir(project_settings_dir):
+		filepath = os.path.join(project_settings_dir, filename)
+		with open(filepath, 'r') as f:
+			text = f.read()
+			text = text.replace(r'##DefaultCompany##', company)
+			text = text.replace(r'##DefaultProduct##', product)
+			text = text.replace(r'##UnityVersion##', unity_version)
+		with open(filepath, "w") as f:
+			f.write(text)
+
+	# Create IDE files
+	copy_template('editorconfig', os.path.join(path, '.editorconfig'))
+	copy_template('Project.sln', os.path.join(path, product + ".sln"))
+	
+	# Initialize Git
+	copy_template('gitignore', os.path.join(path, '.gitignore'))
+	copy_template('gitattributes', os.path.join(path, '.gitattributes'))
+	
 	print('Created project ' + path)
 
