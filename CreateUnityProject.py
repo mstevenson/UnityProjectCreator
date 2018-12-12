8c83e99760f3e6ed3a648438c3bc3baef90a7bfb
Michael Stevenson <michael@mstevenson.net>
2018-12-11 20:53:53 -0800

    Config file, ProjectSettings text replacements

--- CreateUnityProject.py
+++ CreateUnityProject.py
@@ -18,34 +18,68 @@
 import os
 import os.path
 import shutil
+import configparser
 
-template_dir = "templates"
+template_dir = 'templates'
+config = configparser.ConfigParser()
 
 def create_dir(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
 
 def copy_template(source, dest):
-	shutil.copy2(os.path.join(template_dir, source), dest)
+	s = os.path.join(template_dir, source)
+	if (os.path.isfile(s)):
+		shutil.copy2(s, dest)
+	else:
+		shutil.copytree(s, dest)
 
 def initialize_git(path):
-	copy_template("gitignore_template", os.path.join(path, ".gitignore"))
-	copy_template("gitattributes_template", os.path.join(path, ".gitattributes"))
+	copy_template('gitignore', os.path.join(path, '.gitignore'))
+	copy_template('gitattributes', os.path.join(path, '.gitattributes'))
 
-def create_unity_project(path):
+def create_unity_project(path, company, product, unity_version):
 	path = os.path.expanduser(path)
 	if os.path.exists(path):
 		return False
-	create_dir(os.path.join(path, "Assets"))
-	create_dir(os.path.join(path, "ProjectSettings"))
+	create_dir(os.path.join(path, 'Assets'))
+	project_settings_dir = os.path.join(path, 'ProjectSettings')
+	copy_template('ProjectSettings', project_settings_dir)
+	for filename in os.listdir(project_settings_dir):
+		filepath = os.path.join(project_settings_dir, filename)
+		with open(filepath, 'r') as f:
+			text = f.read()
+			text = text.replace(r'%%DefaultCompany%%', company)
+			text = text.replace(r'%%DefaultProduct%%', product)
+			text = text.replace(r'%%UnityVersion%%', unity_version)
+		with open(filepath, "w") as f:
+			f.write(text)
 	return True
 
-if __name__ == "__main__":
-	path = raw_input("Enter a new Unity project folder path: ")
-	success = create_unity_project(path)
+def create_ide_files(path):
+	copy_template('editorconfig', os.path.join(path, '.editorconfig'))
+
+if __name__ == '__main__':
+	config_path = 'config.ini'
+	if os.path.exists(config_path):
+		config.read('config.ini')
+	else:
+		config['Settings'] = {
+			'company' : 'Default Company',
+			'product' : 'Default Product',
+			'unity_version' : '2017.4.17f1',
+			#'timestep' : '0.0111111',
+			#'enable_vr' : 'true'
+		}
+		with open(config_path, 'w') as configfile:
+			config.write(configfile)
+
+	path = raw_input('Enter a new Unity project folder path: ')
+	success = create_unity_project(path, config['Settings']['company'], config['Settings']['product'], config['Settings']['unity_version'])
 	if not success:
 		print('Project already exists')
 		quit()
+	create_ide_files(path)
 	initialize_git(path)
 	print('Created project ' + path)
 
