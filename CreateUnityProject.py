5e6e47f4e536b4661e3998941117b9ff378200d7
Michael Stevenson <michael@mstevenson.net>
2018-12-13 17:22:51 -0800

    Set scene and project GUIDs. Initialize git repo.

--- CreateUnityProject.py
+++ CreateUnityProject.py
@@ -18,9 +18,9 @@
 import os
 import os.path
 import shutil
-import configparser
-
-config = configparser.ConfigParser()
+import uuid
+import argparse
+from git import Repo
 
 def create_dir(path):
 	if not os.path.exists(path):
@@ -34,57 +34,70 @@ def copy_template(source, dest):
 	else:
 		shutil.copytree(s, dest)
 
-if __name__ == '__main__':
-	config_path = 'config.ini'
-	if os.path.exists(config_path):
-		config.read('config.ini')
-	else:
-		config['Settings'] = {
-			'company' : 'Default Company',
-			'unity_version' : '2017.4.17f1',
-			#'timestep' : '0.0111111',
-			#'enable_vr' : 'true'
-		}
-		with open(config_path, 'w') as configfile:
-			config.write(configfile)
+def create_guid():
+	guid = uuid.uuid4()
+	return str(guid).replace('-', '')
 
-	product = raw_input('Enter a Unity project name: ')
+def replace_template_tags(path):
+	# replacements to add:
+	#    - timestep
+	#    - enable_vr
+	with open(path, 'r') as f:
+		text = f.read()
+		text = text.replace(r'##DefaultCompany##', args.company)
+		text = text.replace(r'##DefaultProduct##', args.product)
+		text = text.replace(r'##SceneGuid##', scene_guid)
+		text = text.replace(r'##ProjectGuid##', project_guid)
+		# text = text.replace(r'##UnityVersion##', unity_version)
+	with open(path, "w") as f:
+		f.write(text)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Create an empty Unity project.')
+	parser.add_argument('--company', default='Default Company', type=str)
+	parser.add_argument('product', help='the name of the Unity project')
+	args = parser.parse_args()
 
 	output_dir = "output"
 	create_dir(output_dir)
-	path = os.path.join(output_dir, product)
+	project_path = os.path.join(output_dir, args.product)
 
-	if os.path.exists(path):
-		print('Project already exists')
+	if os.path.exists(project_path):
+		print('Project already exists: ' + project_path)
 		quit()
 
-	create_dir(path)
-
-	company = config['Settings']['company']
-	unity_version = config['Settings']['unity_version']
+	create_dir(project_path)
 
 	# Create Unity project
-	create_dir(os.path.join(path, 'Assets'))
-	project_settings_dir = os.path.join(path, 'ProjectSettings')
+	create_dir(os.path.join(project_path, 'Assets'))
+	project_settings_dir = os.path.join(project_path, 'ProjectSettings')
 	copy_template('ProjectSettings', project_settings_dir)
-	copy_template('Main.unity', os.path.join(path, 'Assets', 'Main.unity'))
+
+	project_guid = create_guid()
+	scene_guid = create_guid()
+
 	for filename in os.listdir(project_settings_dir):
 		filepath = os.path.join(project_settings_dir, filename)
-		with open(filepath, 'r') as f:
-			text = f.read()
-			text = text.replace(r'##DefaultCompany##', company)
-			text = text.replace(r'##DefaultProduct##', product)
-			text = text.replace(r'##UnityVersion##', unity_version)
-		with open(filepath, "w") as f:
-			f.write(text)
+		replace_template_tags(filepath)
+
+	# TODO replace the id in Main.unity.meta with scene_guid var
+	copy_template('Main.unity', os.path.join(project_path, 'Assets', 'Main.unity'))
+	meta_path = os.path.join(project_path, 'Assets', 'Main.unity.meta')
+	copy_template('Main.unity.meta', meta_path)
+	replace_template_tags(meta_path)
 
 	# Create IDE files
-	copy_template('editorconfig', os.path.join(path, '.editorconfig'))
-	copy_template('Project.sln', os.path.join(path, product + ".sln"))
+	copy_template('editorconfig', os.path.join(project_path, '.editorconfig'))
+	copy_template('Project.sln', os.path.join(project_path, args.product + ".sln"))
 	
-	# Initialize Git
-	copy_template('gitignore', os.path.join(path, '.gitignore'))
-	copy_template('gitattributes', os.path.join(path, '.gitattributes'))
+	# Initialize Git project
+	copy_template('gitignore', os.path.join(project_path, '.gitignore'))
+	copy_template('gitattributes', os.path.join(project_path, '.gitattributes'))
+
+	repo = Repo.init(project_path)
+	#repo.create_submodule
+	repo.index.add('*')
+	repo.commit('Created Unity project ' + args.product)
 	
-	print('Created project ' + path)
+	print('Created Unity project: ' + project_path)
 
